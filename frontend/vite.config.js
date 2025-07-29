af079158251de479d0b9417fc27e279354388d2c
M K Lakshmi <134765745+Lakshmi-M-K@users.noreply.github.com>
2025-07-29 19:31:09 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 This ensures proper routing & build path
   plugins: [react()],
 })
