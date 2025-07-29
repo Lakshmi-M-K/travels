9ab8829ab7b3dbb71d7efe12a098878d61fc94b7
M K Lakshmi <134765745+Lakshmi-M-K@users.noreply.github.com>
2025-07-29 19:25:59 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "travels-backend-production-479b.up.railway.app";
+const BASE_URL = "https://travels-backend-production-479b.up.railway.app/";
 // const BASE_URL = "http://localhost:3050/api";
 //
 export default BASE_URL;
