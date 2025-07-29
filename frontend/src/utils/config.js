1a09c19008c997a1d596785dc73fab556d325ee7
M K Lakshmi <134765745+Lakshmi-M-K@users.noreply.github.com>
2025-07-29 19:11:21 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://travels-backend-production-479b.up.railway.app";
+const BASE_URL = "https://travels-jqbqe3ele-manoj-gowdas-projects-5dd01787.vercel.app";
 // const BASE_URL = "http://localhost:3050/api";
 //
 export default BASE_URL;
