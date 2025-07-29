c012f015eca1918e1f9cd70ae0319295ae2e41ee
M K Lakshmi <134765745+Lakshmi-M-K@users.noreply.github.com>
2025-07-29 18:01:09 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://trips-travel-server.vercel.app/api";
+const BASE_URL = "travels-chi-coral.vercel.app";
 // const BASE_URL = "http://localhost:3050/api";
 //
 export default BASE_URL;
